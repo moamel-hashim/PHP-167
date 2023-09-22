4b12e60144167700255adf3e63f527289785e9b5
moamel-hashim <muamal1997@gmail.com>
2023-09-21 18:26:30 -0700

    fixed some errors

--- fixingError.php
+++ fixingError.php
@@ -5,14 +5,17 @@ $cash = 31;
 $dinner = 25;
 $tax = 10;
 $tip = 10;
-while(($cost = restaurant_check($dinner,$tax,$tip)) < $cash) {
-$tip++;
-echo 'I can afford a tip of '.$tip.'% ('.$cost.')'."\n";
-}
+
 function restaurant_check($meal, $tax, $tip) {
-$tax_amount = $meal * ($tax / 100);
-$tip_amount = $meal * ($tip / 100);
-return $meal + $tax_amount + $tip_amount;
+  $tax_amount = $meal * ($tax / 100);
+  $tip_amount = $meal * ($tip / 100);
+  return $meal + $tax_amount + $tip_amount;
 }
+
+while(($cost = restaurant_check($dinner,$tax,$tip)) < $cash) {
+  $tip++;
+  echo 'I can afford a tip of '.$tip.'% ('.$cost.')'."\n";
+}
+
 
 ?>
